dbe0d88b369692dd54524be8f145cc9db1f85c29
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-04-03 16:41:18 +0200

    :bee: adding and styling SummaryBalanceCard component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,13 +2,16 @@ import Avatar from "../../components/Avatar/Avatar";
 import BalanceCard from "../../components/BalanceCard/BalanceCard";
 import { Link } from "react-router-dom";
 import React from "react";
+import SummaryBalanceCard from "../../components/SummaryBalanceCard/SummaryBalanceCard";
 import TransactionListItem from "../../components/TransactionListItem/TransactionListItem";
 
 const Home: React.FC = () => {
 	return (
-		<div className="container">
+		<div>
+			<SummaryBalanceCard balanceDifference={443} amountBalance={62.18}></SummaryBalanceCard>
 			<h1>Home</h1>
 			<Link to="/test">Go test</Link>
+
 			<Avatar height={40} width={40}></Avatar>
 			<BalanceCard balanceTitle={"Invest debt"} balanceAmount={800} currentBalanceAmount={2000}></BalanceCard>
 			<TransactionListItem
